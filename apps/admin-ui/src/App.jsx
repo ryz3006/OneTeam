4a869f2eb5983e3369ac98476bc555356bcb592b
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-09 15:26:42 +0530

    Update App.jsx

--- apps/admin-ui/src/App.jsx
+++ apps/admin-ui/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AdminLoginPage from './pages/AdminLoginPage';
-import AdminDashboardPage from './pages/AdminDashboardPage'; // You will create this
+import AdminDashboardPage from './pages/AdminDashboardPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AdminLoginPage />} />
-      <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
-      {/* Add other admin routes here */}
+      <Route path="/dashboard" element={<AdminDashboardPage />} />
+      {/* Add other admin routes for Project Management, Users, etc. here */}
     </Routes>
   );
 }
