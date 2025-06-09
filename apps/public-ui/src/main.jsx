68a5bdb435fce127056ec4fff8d2db1d2e69fe25
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-09 15:04:56 +0530

    Update main.jsx

--- apps/public-ui/src/main.jsx
+++ apps/public-ui/src/main.jsx
@@ -1,13 +1,17 @@
+// File: apps/public-ui/src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+// Change this import from BrowserRouter to HashRouter
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* Change this component from BrowserRouter to HashRouter */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
