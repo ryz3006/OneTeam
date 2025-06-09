8bbe3733093db3e1e7539e6145615394e573afcd
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-09 15:26:27 +0530

    Update main.jsx

--- apps/admin-ui/src/main.jsx
+++ apps/admin-ui/src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
